4ec9a02870b28b6aa99bb22721e07631fb5c4950
sad4itananda <4ipej6ij@mail.ru>
2018-06-12 09:26:36 +0300

    some probels

--- js/stat.js
+++ js/stat.js
@@ -10,17 +10,17 @@ var COLUMN_WIDTH = 40;
 var drawFigure = function (shiftX, shiftY, color, ctx) {
   ctx.fillStyle = color;
   ctx.beginPath ();
-  ctx.moveTo (START_X + shiftX, START_Y + shiftY);
-  ctx.lineTo (START_X + shiftX + 210, START_Y + shiftY - 10);
-  ctx.lineTo (START_X + shiftX + 420, START_Y + shiftY + 0);
-  ctx.lineTo (START_X + shiftX + 410, START_Y + shiftY + 145);
-  ctx.lineTo (START_X + shiftX + 420, START_Y + shiftY + 280);
-  ctx.lineTo (START_X + shiftX + 200, START_Y + shiftY + 270);
-  ctx.lineTo (START_X + shiftX + 0,   START_Y + shiftY + 280);
-  ctx.lineTo (START_X + shiftX + 10,  START_Y + shiftY + 145);
-  ctx.lineTo (START_X + shiftX + 0,   START_Y + shiftY + 10);
-  ctx.fill ();
-  ctx.closePath ();
+  ctx.moveTo(START_X + shiftX, START_Y + shiftY);
+  ctx.lineTo(START_X + shiftX + 210, START_Y + shiftY - 10);
+  ctx.lineTo(START_X + shiftX + 420, START_Y + shiftY + 0);
+  ctx.lineTo(START_X + shiftX + 410, START_Y + shiftY + 145);
+  ctx.lineTo(START_X + shiftX + 420, START_Y + shiftY + 280);
+  ctx.lineTo(START_X + shiftX + 200, START_Y + shiftY + 270);
+  ctx.lineTo(START_X + shiftX + 0, START_Y + shiftY + 280);
+  ctx.lineTo(START_X + shiftX + 10, START_Y + shiftY + 145);
+  ctx.lineTo(START_X + shiftX + 0, START_Y + shiftY + 10);
+  ctx.fill();
+  ctx.closePath();
 };
 
 var getMaxTime = function (times) {
@@ -48,24 +48,24 @@ var drawGistogramm = function (ctx, names, times) {
     var anotherColor = '\'rgba(0, 0, 255, ' + random + ')\'';
     var columnHeight = times[i] / maxColumn * 150;
     if (names[i] == 'Вы') {
-      drawColumns ('rgba(255, 0, 0, 1)', columnHeight, ctx, names, times, i, CoordX);
+      drawColumns('rgba(255, 0, 0, 1)', columnHeight, ctx, names, times, i, CoordX);
     } else {
-      drawColumns (anotherColor, columnHeight, ctx, names, times, i,CoordX);
+      drawColumns(anotherColor, columnHeight, ctx, names, times, i,CoordX);
     }
     CoordX += SHIFT_COLUMN;
   };
 };
 
 window.renderStatistics = function (ctx, names, times) {
-  drawFigure (10, 10, 'rgba(0, 0, 0, 0.7)', ctx);
-  drawFigure (0, 0, 'rgba(255, 255, 255, 1)', ctx);
+  drawFigure(10, 10, 'rgba(0, 0, 0, 0.7)', ctx);
+  drawFigure(0, 0, 'rgba(255, 255, 255, 1)', ctx);
 
   ctx.fillStyle = 'black';
   ctx.font = '16px PT Mono';
-  ctx.fillText ('Ура вы победили!', 240, 30);
-  ctx.fillText ('Список результатов:', 230, 60);
+  ctx.fillText('Ура вы победили!', 240, 30);
+  ctx.fillText('Список результатов:', 230, 60);
 
-  drawGistogramm (ctx, names, times);
+  drawGistogramm(ctx, names, times);
 };
 
 
